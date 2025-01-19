16977c72811bc74a24a8db33c5d80becdeccb6b1
jhhocs <joshuaho1@cpp.edu>
2025-01-19 00:57:13 -0800

    Read incomming data

--- Communication/XBee.py
+++ Communication/XBee.py
@@ -1,7 +1,8 @@
 import serial
+from Communication.interfaces.Serial import Serial
 # import multiprocessing
 
-class XBee:
+class XBee(Serial):
     # Initialize serial connection
     def __init__(self, port, baudrate):
         """Initialize serial connection
@@ -14,6 +15,7 @@ class XBee:
         self.baudrate = baudrate
         self.ser = None
         self.__transmitting = False
+        self.__receiving = False
     
 
     def open(self):
@@ -60,17 +62,56 @@ class XBee:
         return False
 
 
-    def receive_data(self):
+    def retrieve_data(self):
         """Read incomming data.
 
         Returns:
-          Incomming String data, None if no data.
+          Incomming String data (Deframed data), None if no data.
         """
-        data = self.ser.readline()
-        if data == None:
-            return None
-        data = self.__decode_data(data)
-        return data
+        # bytes = self.ser.readline()
+
+        # if bytes:
+        #   print(''.join('{:02x} '.format(x) for x in bytes))
+
+        byte = self.ser.read(1)
+
+        # Read byte by byte until start delimiter (7E) is read
+        if byte and byte[0] == 0x7E:
+            # # print(hex(byte[0]))
+            # print('{:02x} '.format(byte[0]))
+            self.__receiving = True
+            # # It might be possible for nothing to be read, leading to an incorrect length
+            l1 = self.ser.read(1)
+            l2 = self.ser.read(1)
+            print("Length bytes:",'{:02x} '.format(l1[0]), '{:02x} '.format(l2[0]))
+            length = l1[0] * 256 + l2[0]
+            print("Length:", length)
+            data = b''
+            while len(data) < length:
+                chunk = self.ser.read(length - len(data))
+                data += chunk
+            print("Data:")
+            for b in data:
+                print('{:02x} '.format(b))
+                # print("Data:", data)
+            while True:
+                expected_checksum = self.ser.read(1)
+                if expected_checksum:
+                    print("Received Checksum:", '{:02x} '.format(expected_checksum[0]))
+                    calculated_checksum = 0xFF - (sum(data) & 0xFF)
+                    print("Calculated Checksum:", '%0.2x' % calculated_checksum)
+                    break
+            # return self.__decode_data()
+        elif byte:
+        #     print(data, data[0])
+        #     print(data[0] == 0x7E)
+        #     print("Incomming framed data:")
+            # print(byte)
+            print('Pass {:02x} '.format(byte[0]))
+        #     data = self.__decode_data(data)
+            # print("Decoded data:" + data)
+            # return data
+        return None
     
 
     # NOTE** Might need to check data length
@@ -96,35 +137,37 @@ class XBee:
 
         frame.append(0x00)  # Options (1 byte)
         frame.extend(data.encode('utf-8'))  # RF data (0 - 256 bytes)
+        # FF - number of bytes between length & checksum field
         checksum = 0xFF - (sum(frame[3:]) & 0xFF)
         frame.append(checksum)  # Checksum (1 byte)
 
-        print(frame)
+        # print(frame)
+        print(''.join('{:02x} '.format(x) for x in frame))
 
         return frame
     
+    # Frames may be sent separately (different lines)
+    # https://pyserial.readthedocs.io/en/latest/pyserial_api.html
+    # 1. Read byte by byte until 7E (~) is read. -> read(1)
+    # 2. Read next 2 bytes to determine frame size (length) -> read(2)
+    # 3. Read next "length" bytes -> read(length)
+    # 4. Read final byte for checksum-> read(1)
+    # NOTE** Might be able to use read_until(exptected, size) https://pyserial.readthedocs.io/en/latest/pyserial_api.html#serial.Serial.read_until
+    # 5. Check if calculated checksum is equal to the read checksum value
+    # def __decode_data(self, data): 
+    #     """Decode String data.
 
-    def __decode_data(self, data): # NOTE** Might need to retrieve sender address
-        """Decode String data.
+    #     Args: 
+    #       data: String data to decode.
 
-        Args: 
-          data: String data to decode.
-
-        Returns:
-          Deframed String data.
-        """
-        message = data[15:-1].decode('utf-8')
-        return message
-
-    # # Read next incoming data (Include timeout?)
-    # def read_next(self):
-    #     try:
-    #         while True:
-    #             data = self.receive_data()
-    #             if data:
-    #                 # print(f"Received data: {data}")
-    #                 return data
-
-    #     except serial.SerialException as e:
-    #         print(f"Error: {e}")
-    #         return False
+    #     Returns:
+    #       Deframed String data.
+    #     """
+    #     # print("data: ", data[1])
+    #     print(data[1], data[2])
+    #     length = (data[1] * 256) + data[2]
+    #     print("Length:", length)
+    #     # for byte in data:
+    #     #     print(byte)
+    #     message = data[15:-1].decode('utf-8')
+    #     return message
---
+++ Communication/interfaces/Serial.py
@@ -0,0 +1,35 @@
+class Serial:
+    def open(self) -> bool:
+        """Opens serial port.
+
+        Returns:
+          True if success, False if failure.
+        """
+        pass
+
+    def close(self) -> bool:
+        """Close serial port.
+
+        Returns:
+          True if success, False if failure (Error or port already closed).
+        """
+        pass
+
+    def transmit_data(self, data) -> bool:
+        """Transmit data.
+
+        Args:
+          data: String data to transmit.
+
+        Returns:
+          True if success, False if failure.
+        """
+        pass
+
+    def retrieve_data(self):
+        """Read incomming data.
+
+        Returns:
+          Incomming String data (Deframed data), None if no data.
+        """
+        pass
