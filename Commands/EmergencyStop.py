2ce4a5ccadc207d23fd0ad0c52fc8d7f611f7f35
jhhocs <joshuaho1@cpp.edu>
2025-05-14 16:29:37 -0700

    refactor: Import encode and decode methods instead of entire class

--- Commands/EmergencyStop.py
+++ Commands/EmergencyStop.py
@@ -1,36 +1,36 @@
-from Interfaces.CommandInterface import CommandInterface
+# from Interfaces.CommandInterface import CommandInterface
 import struct
 
-class EmergencyStop(CommandInterface):
-    def __init__(self):
-        self.FORMAT_STRING = "BBB"
-        self.PAYLOAD_ID = 1 # All commands will have a payload ID of 1
-        self.COMMAND_ID = 1
+# class EmergencyStop(CommandInterface):
+#     def __init__(self):
+FORMAT_STRING = "BBB"
+PAYLOAD_ID = 1 # All commands will have a payload ID of 1
+COMMAND_ID = 1
 
-    def encode_data(self, stop_status) -> str:
-        """Encode data packet
+def encode_emergency_stop(stop_status) -> str:
+    """Encode data packet
 
-        Args:
-          stop_status: Status for emergency stop: 0 = Enable Emergency Stop, 1 = Disable Emergency Stop
+    Args:
+        stop_status: Status for emergency stop: 0 = Enable Emergency Stop, 1 = Disable Emergency Stop
 
-        Returns:
-          Encoded data string
-        """
-        encoded_string = struct.pack(self.FORMAT_STRING, self.PAYLOAD_ID, self.COMMAND_ID, stop_status)
-        return encoded_string
+    Returns:
+        Encoded data string
+    """
+    encoded_string = struct.pack(FORMAT_STRING, PAYLOAD_ID, COMMAND_ID, stop_status)
+    return encoded_string
 
-    def decode_data(self, encoded_string) -> str:
-        """Decodes data packet
-        
-        Args:
-          Encoded data packet
+def decode_emergency_stop(encoded_string) -> str:
+    """Decodes data packet
+    
+    Args:
+        Encoded data packet
 
-        Returns:
-          Data from encoded data packet
-        """
-        expected_size = 3
-        if len(encoded_string) != expected_size:
-            print("Invalid String Size")
-        unpacked_data = struct.unpack(self.FORMAT_STRING, encoded_string)
+    Returns:
+        Data from encoded data packet
+    """
+    expected_size = 3
+    if len(encoded_string) != expected_size:
+        print("Invalid String Size")
+    unpacked_data = struct.unpack(FORMAT_STRING, encoded_string)
 
-        return unpacked_data[2]
+    return unpacked_data[2]
--- Commands/test.py
+++ Commands/test.py
@@ -1,15 +1,15 @@
-from EmergencyStop import EmergencyStop
+from EmergencyStop import encode_emergency_stop, decode_emergency_stop
 
-ES = EmergencyStop()
+# ES = EmergencyStop()
 
 print("Enabling Emergency Stop: ")
 
-input = 0
+input = 1
 
-encoded_data = ES.encode_data(input)
+encoded_data = encode_emergency_stop(input)
 
 print(f"Expected string: '01 01 0{input}' Actual string: " + " ".join(f"{b:02x}" for b in encoded_data))
 
-decoded_data = ES.decode_data(encoded_data)
+decoded_data = decode_emergency_stop(encoded_data)
 
 print(decoded_data)
