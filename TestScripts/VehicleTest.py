9aa07aa408173f9518196f82da6d51cfc298d23a
Feniren <112586282+Feniren@users.noreply.github.com>
2026-04-23 03:29:48 -0700

    Development (#44)
    * Fixed Typo
    
    * Updated documentation
    
    * Updated Documentation
    
    * Updated Documentation. Updated test scripts
    
    * Removed old code
    
    * Added test files
    
    * Updated InfrastructureInterface

--- TestScripts/VehicleTest.py
+++ TestScripts/VehicleTest.py
@@ -16,25 +16,29 @@ PacketLibrary.SetGCSMACAddress("0013A2004283A0EC")
 
 LaunchVehicleXBee(PORT)
 
-Command1 = ReceiveCommand()
-Command2 = ReceiveCommand()
-Command3 = ReceiveCommand()
+Command1 = ReceiveCommand(DecodeFormat.Class)
+Command2 = ReceiveCommand(DecodeFormat.Class)
+Command3 = ReceiveCommand(DecodeFormat.Class)
 
 print(Command1)
 print(Command2)
 print(Command3)
 
-try:
-    CommandData1 = json.loads(Command1)
-    CommandData2 = json.loads(Command2)
-    CommandData3 = json.loads(Command3)
+#try:
+    #CommandData1 = json.loads(Command1)
+    #CommandData2 = json.loads(Command2)
+    #CommandData3 = json.loads(Command3)
                     
-except json.JSONDecodeError as e:
-    print(f"JSON Error: {e}")
+#except json.JSONDecodeError as e:
+    #print(f"JSON Error: {e}")
 
-Telemetry1 = Telemetry(CommandData1["Command ID"], CommandData1["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
-Telemetry2 = Telemetry(CommandData2["Command ID"], CommandData2["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
-Telemetry3 = Telemetry(CommandData3["Command ID"], CommandData3["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+#Telemetry1 = Telemetry(CommandData1["Command ID"], CommandData1["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+#Telemetry2 = Telemetry(CommandData2["Command ID"], CommandData2["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+#Telemetry3 = Telemetry(CommandData3["Command ID"], CommandData3["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+
+Telemetry1 = Telemetry(Command1.COMMAND_ID, Command1.PacketID, 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+Telemetry2 = Telemetry(Command2.COMMAND_ID, Command2.PacketID, 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+Telemetry3 = Telemetry(Command3.COMMAND_ID, Command3.PacketID, 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
 
 SendTelemetry(Telemetry1)
 SendTelemetry(Telemetry2)
@@ -43,32 +47,34 @@ SendTelemetry(Telemetry3)
 # End of the example. The rest is for live command receiving
 
 while (True):
-    Command = ReceiveCommand()
+    Command = ReceiveCommand(DecodeFormat.Class)
 
     print(type(Command))
 
     try:
-        CommandData = json.loads(Command)
+        if (isinstance(Command, str)):
+            CommandData = json.loads(Command)
         
         match (Command):
             case Heartbeat():
                 print("Executing Heartbeat code")
             case EmergencyStop():
                 print("Executing Emergency Stop code")
-            case KeepIn():
+            case AddZone():
                 print("Executing Keep In code")
-            case KeepOut():
-                print("Executing Keep Out code")
             case PatientLocation():
                 print("Executing Patient Location code")
-            case SearchArea():
-                print("Executing Search Area code")
             case _:
                 print("Unknown Command Type")
                     
     except json.JSONDecodeError as e:
         print(f"JSON Error: {e}")
 
-    TelemetryInstance = Telemetry(CommandData["Command ID"], CommandData["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+    print(f"{Command.COMMAND_ID} {Command.PacketID} {Command.PACKET_ID}")
+    
+    if (isinstance(Command, str)):
+        TelemetryInstance = Telemetry(CommandData["Command ID"], CommandData["Packet ID"], 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
+    else:
+        TelemetryInstance = Telemetry(Command.COMMAND_ID, Command.PacketID, 100, 0, 0, 0, 45, 0.5, 0, (1, 2), 0, 0, 1.0, 1.0, 0)
 
-    SendTelemetry(Telemetry1)
+    SendTelemetry(TelemetryInstance)
